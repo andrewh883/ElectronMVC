90d2b055f8ec8f522370f63c029c9a02d5c61c66
Andrew Hurst <andrew.hurst96@gmail.com>
2020-11-30 20:56:23 -0500

    Modified

--- ElectronMVCApplication/Controllers/HomeController.cs
+++ ElectronMVCApplication/Controllers/HomeController.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ElectronMVCApplication.Models;
 using ElectronMVCApplication.Data;
+using Microsoft.PowerPlatform.Cds.Client;
+using System.Security;
+using System.Net;
+using System.Net.Http;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace ElectronMVCApplication.Controllers
 {
@@ -28,9 +35,45 @@ namespace ElectronMVCApplication.Controllers
 
         public IActionResult Privacy()
         {
-            var test = dbContext.Database.CanConnect();
-            return Json(test);
+            //var test = dbContext.Database.CanConnect();
+            //return Json(test);
             //return View();
+            string serviceUrl = "https://yourorg.crm.dynamics.com";
+            string clientId = "51f81489-12ee-4a9e-aaae-a2591f45987d";
+            string userName = "you@yourorg.onmicrosoft.com";
+            string password = "yourpassword";
+
+            AuthenticationContext authContext =
+                new AuthenticationContext("https://login.microsoftonline.com/common", false);
+            UserCredential credential = new UserCredential(userName);
+            AuthenticationResult result = authContext.AcquireTokenAsync(serviceUrl, clientId, credential).Result;
+            //The access token
+            string accessToken = result.AccessToken;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(serviceUrl);
+                client.Timeout = new TimeSpan(0, 2, 0);  //2 minutes  
+                client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpRequestMessage request =
+                    new HttpRequestMessage(HttpMethod.Get, "/api/data/v9.0/WhoAmI");
+                //Set the access token
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = client.SendAsync(request).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    //Get the response content and parse it.  
+                    JObject body = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                    Guid userId = (Guid)body["UserId"];
+                    Console.WriteLine("Your system user ID is: {0}", userId);
+                }
+
+                return View();
+            }
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
